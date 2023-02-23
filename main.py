2988d0f153dddeb3bde4dca305b8cc0304a3a8d9
DanielMeads <117637190+DanielMeads@users.noreply.github.com>
2023-02-24 00:03:59 +0100

    Changes to be committed: 	modified:   main.py 	modified:   test.py

--- main.py
+++ main.py
@@ -3,69 +3,94 @@ __human_name__ = "Betsy Webshop"
 
 import models
 import peewee
+from peewee import fn, JOIN
 from textblob import TextBlob
 from models import Tag, Product, User, Sale, UserProduct, ProductTag
 
+# Get database id information for further use
+def get_user_id(user=1):
+    if type(user) == str:
+        return User.get(User.name == user)
+    elif type(user) == int:
+        return User.get(User.id == user)
+
+
+def get_product_id(product=1):
+    if type(product) == str:
+        return User.get((fn.Lower(Product.name)) == (fn.Lower(product)))
+    elif type(product) == int:
+        return Product.get(Product.id == product)
+
+
+def get_tag_id(tag):
+    return Tag.get(Tag.tag == tag)
+
+
+# Main searches
 def search(term):
+    #Attempts spellcheck
     tospellcheck = TextBlob(term.lower())
-    term = str(tospellcheck.correct())
-    query = Product.select()
-    results = []
-    for product in query:
-        if (product.name.lower() == term) or (term in product.description.lower()):
-            if product.amount_in_stock > 0:
-                results.append([product.name, (product.unit_price_cents/100), product.amount_in_stock])
-    print(results)
+    term2 = str(tospellcheck.correct())
+    query = Product.select().where(
+        (fn.Lower(term) % fn.Lower(Product.name)) | term2 % fn.Lower(Product.name)
+    )
+    return [[product.name, product.unit_price_cents / 100, product.amount_in_stock] for product in query]
 
 
-def list_user_products(user_id):
-    userproducts = UserProduct.select().where(UserProduct.user == user_id)
-    results = []
-    for product in userproducts:
-        product1 = Product.get(product.id == Product.id)
-        results.append([product1.name, (product1.unit_price_cents/100), product1.amount_in_stock])
-    print(results)
+def list_user_products(user):
+    userproducts = (
+        Product.select()
+        .join(UserProduct, JOIN.LEFT_OUTER)
+        .where(UserProduct.user_id == user_id)
+    )
+    return [product.name for product in userproducts]
 
 
-def list_products_per_tag(tag_id):
-    productswithtag = ProductTag.select().where(ProductTag.tag == tag_id)
-    results = []
-    for product in productswithtag:
-        product1 = Product.get(Product.id == product.product.id)
-        if product1.amount_in_stock > 0:
-            results.append([product1.name, (product1.unit_price_cents/100), product1.amount_in_stock])
-    print(results)
+def list_products_per_tag(tag):
+    #Tag must be a known tag name or id i.e. 'Ugly' or 'Wool'
+    id = get_tag_id(tag)
+    productswithtag = ProductTag.select(ProductTag.product_id).where(
+        ProductTag.tag_id == id
+    )
+    products = Product.select().where(Product.id.in_(productswithtag))
+    return [product.name for product in products]
 
 
 def add_product_to_catalog(user_id, product):
-    new_product = Product.create(name=product[0], description=product[1], unit_price_cents=product[3], amount_in_stock=product[4])
+    # Requires a user name or ID i.e "Trevor" and a List of product information i.e.['ProductName', 'Description', [List of Tags], Price in cents, Amount]
+    new_product = Product.create(
+        name=product[0],
+        description=product[1],
+        unit_price_cents=product[3],
+        amount_in_stock=product[4],
+    )
     UserProduct.create(user=user_id, product=new_product)
+    # Creates tags or returns an existing tags id
     for tagname in product[2]:
         try:
             tag = Tag.create(tag=tagname)
         except:
             tag = Tag.get(Tag.tag == tagname)
+        #Logs the Products Tags
         ProductTag.create(product=new_product, tag=tag)
 
 
-def update_stock(product_id, new_quantity):
-    query = product_id.update(amount_in_stock=new_quantity).where(Product.id == product_id)
-    query.execute()
-
-
 def purchase_product(product_id, buyer_id, quantity):
+    # Takes a product name or id i.e 'Pants' or 2, a buyer name or id i.e "Trevor" or 1, and a quantity
+    product_id = get_product_id(product_id)
+    buyer_id = get_user_id(buyer_id)
+    # Find seller ID
     seller_id = UserProduct.get(UserProduct.product == product_id)
     new_sale = Sale.create(buyer=buyer_id, seller=seller_id.user, product_purchased=product_id, quantity=quantity)
     newquantity = product_id.amount_in_stock - quantity
     if newquantity < 1:
-        remove_product(product_id)
+        # Removes the product from sale
+        query = product_id.update(amount_in_stock=0).where(Product.id == product_id) 
+        query.execute()   
+        #Removes the product from the User so will no longer be referenced
+        query2 = UserProduct.delete().where(UserProduct.product == product_id)
+        query2.execute() 
     else:
-        update_stock(product_id, newquantity)
-
-
-def remove_product(product_id):
-    query1 = product_id.update(amount_in_stock=0).where(Product.id == product_id) 
-    query1.execute()   
-    query2 = UserProduct.delete().where(UserProduct.product == product_id)
-    query2.execute() 
-
+        # Adjusts the stock
+        query = product_id.update(amount_in_stock=newquantity).where(Product.id == product_id)
+        query.execute()
--- test.py
+++ test.py
@@ -3,9 +3,18 @@ import peewee
 import models
 import unittest
 from rich.console import Console
+
 console = Console()
 
-Models = [models.Tag, models.Product, models.User, models.Sale, models.UserProduct, models.ProductTag]
+Models = [
+    models.Tag,
+    models.Product,
+    models.User,
+    models.Sale,
+    models.UserProduct,
+    models.ProductTag,
+]
+
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
@@ -19,78 +28,103 @@ class BaseTestCase(unittest.TestCase):
 
 
 def create_test_users():
-    test_users= [
-        ['test1@test1.com', 'Trevor', '1 Testlane Test City', 'Testcreditcard'],
-        ['test2@test2.com', 'Andrew', '2 Testlane Test City', 'Testcreditcard'],
-        ['test3@test3.com', 'Sidney', '3 Testlane Test City', 'Testcreditcard'],
-        ['test4@test4.com', 'John', '4 Testlane Test City', 'Testcreditcard'],
-        ['test5@test5.com', 'Greg', '5 Testlane Test City', 'Testcreditcard'],
-        ['test6@test6.com', 'Edward', '6 Testlane Test City', 'Testcreditcard']
+    test_users = [
+        ["test1@test1.com", "Trevor", "1 Testlane Test City", "Testcreditcard"],
+        ["test2@test2.com", "Andrew", "2 Testlane Test City", "Testcreditcard"],
+        ["test3@test3.com", "Sidney", "3 Testlane Test City", "Testcreditcard"],
+        ["test4@test4.com", "John", "4 Testlane Test City", "Testcreditcard"],
+        ["test5@test5.com", "Greg", "5 Testlane Test City", "Testcreditcard"],
+        ["test6@test6.com", "Edward", "6 Testlane Test City", "Testcreditcard"],
     ]
     for test_user in test_users:
-        models.User.create(email=test_user[0], name=test_user[1], address=test_user[2], billing_information=test_user[3])
-    console.print('Users Created', style='green bold')
-    
+        models.User.create(
+            email=test_user[0],
+            name=test_user[1],
+            address=test_user[2],
+            billing_information=test_user[3],
+        )
+    console.print("Users Created", style="green bold")
+
 
 def get_user_id(number=1):
-        return models.User.get(models.User.id == number)
+    return models.User.get(models.User.id == number)
 
 
 def get_product_id(number=1):
-        return models.Product.get(models.Product.id == number)
+    return models.Product.get(models.Product.id == number)
 
 
 def get_tag_id(tag):
-        return models.Tag.get(models.Tag.tag == tag)
+    return models.Tag.get(models.Tag.tag == tag)
 
 
 def add_test_product():
     test_products= [
-        [get_user_id(3), ['sWeaTER', 'UglY ChRIstMas SweatTer', ['Ugly', 'Wool', 'Red', 'Red'], 256, 36]],
-        [get_user_id(6), ['PAntS', 'UglY ChRIstMas Trousers', ['Ugly', 'Ployester', 'Green'], 120, 2]],
-        [get_user_id(2), ['PAntS', 'UglY ChRIstMas Shorts', ['Ugly', 'Cotton'], 95, 2]],
-        [get_user_id(1), ['ShirT', 'UglY ChRIstMas Shirt', ['Ugly', 'Wool'], 4578, 12]],
-        [get_user_id(1), ['ScaRf', 'UglY ChRIstMas Scarf', ['Ugly'], 2453, 4]],
-        [get_user_id(5), ['Scarf', 'UglY ChRIstMas Scarf with ribbons on', ['Ugly', 'Nylon', 'Orange', 'Blue'], 1199, 1]]
+        [3, ['sWeaTER', 'UglY ChRIstMas SweatTer', ['Ugly', 'Wool', 'Red', 'Red'], 256, 36]],
+        ["Edward", ['PAntS', 'UglY ChRIstMas Trousers', ['Ugly', 'Ployester', 'Green'], 120, 2]],
+        [2, ['PAntS', 'UglY ChRIstMas Shorts', ['Ugly', 'Cotton'], 95, 2]],
+        ["Trevor", ['ShirT', 'UglY ChRIstMas Shirt', ['Ugly', 'Wool'], 4578, 12]],
+        [1, ['ScaRf', 'UglY ChRIstMas Scarf', ['Ugly'], 2453, 4]],
+        ["Greg", ['Scarf', 'UglY ChRIstMas Scarf with ribbons on', ['Ugly', 'Nylon', 'Orange', 'Blue'], 1199, 1]]
     ]
     for product in test_products:
-        main.add_product_to_catalog(product[0],product[1])
-    console.print('Products Created', style='green bold')
+        main.add_product_to_catalog(product[0], product[1])
+    console.print("Products Created", style="green bold")
 
 
 def add_test_purchases():
     test_purchases= [
-        [get_product_id(3), get_user_id(1), 1],
-        [get_product_id(4), get_user_id(5), 4],
-        [get_product_id(2), get_user_id(2), 1],
-        [get_product_id(1), get_user_id(5), 15],
+        [3, 1, 1],
+        [4, 5, 4],
+        [2, 2, 1],
+        [1, 5, 15],
     ]
     for purchase in test_purchases:
         main.purchase_product(purchase[0], purchase[1], purchase[2])
-    console.print('Purchases Enacted', style='green bold')
+    console.print("Purchases Enacted", style="green bold")
 
 
 def add_test_purchases2():
-    test_purchases= [
+    test_purchases = [
         [get_product_id(3), get_user_id(6), 1],
     ]
     for purchase in test_purchases:
         main.purchase_product(purchase[0], purchase[1], purchase[2])
-    console.print('Purchase Enacted', style='green bold')
+    console.print("Purchase Enacted", style="green bold")
 
 
-#BaseTestCase.setUp(BaseTestCase)
-#create_test_users()
-#add_test_product()
-#add_test_purchases()
-#add_test_purchases2()
-#print (main.search('ChrisstmaS'))
-print (main.search('sWeaTER'))
-#main.search('scarf')
-#main.list_user_products(get_user_id(1))
-#main.list_products_per_tag(get_tag_id('Ugly'))
-#print([product for product in models.Product.select().join(models.UserProduct).where(models.UserProduct.user == get_user_id(2))])
-#print([tag.tag for tag in models.Tag.select()])
-#print([product.amount_in_stock for product in models.Product.select()])
-#print([product for product in models.UserProduct.select()])
-#BaseTestCase.tearDown(BaseTestCase)
+# Connect to database (DONT SWITCH OFF!)
+BaseTestCase.setUp(BaseTestCase)
+
+# Create content in database
+# create_test_users()
+# add_test_product()
+# add_test_purchases()
+# add_test_purchases2()
+
+# Search in name case insensitive and attempts spellcheck
+# print (main.search('ShiRt'))
+# print (main.search('sWeaTeeR'))
+# print (main.search('scarf'))
+
+# Search in Description as well ?? - not working
+# print (main.search('TroUSerS'))
+
+# List of user products
+# print (main.list_user_products(1))
+# print (main.list_user_products('Edward'))
+# print (main.list_user_products('Trevor'))
+
+# List of products per tag
+# print (main.list_products_per_tag('Ugly'))
+# print (main.list_products_per_tag('Wool'))
+
+# Check Tags Unique
+# print([tag.tag for tag in models.Tag.select()])
+
+# Check Stock Amount Correct and linked to seller
+# print([product.amount_in_stock for product in models.Product.select()])
+# print([product for product in models.UserProduct.select()])
+
+# Clear the test database
+# BaseTestCase.tearDown(BaseTestCase)
